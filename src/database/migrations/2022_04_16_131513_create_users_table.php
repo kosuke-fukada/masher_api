362d6e8de30a9b34c433bf7f6f107b81cdd9e120
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-04-16 23:59:11 +0900

    fixed user model and removed migration files.

--- src/database/migrations/2022_04_16_131513_create_users_table.php
+++ src/database/migrations/2022_04_16_131513_create_users_table.php
@@ -15,11 +15,12 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
-            $table->rememberToken();
+            $table->string('account_id', 30)->nullable(false);
+            $table->string('display_name', 50)->nullable(false);
+            $table->string('avatar')->nullable(false);
+            $table->string('access_token')->nullable(false);
+            $table->string('refresh_token')->nullable(false);
+            $table->string('provider')->nullable(false);
             $table->timestamps();
         });
     }
