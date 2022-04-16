8184c4d4e92f88f93809c81ad0ed2537a21be2f2
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-04-16 23:57:20 +0900

    added testing config.

--- src/tests/TestCase.php
+++ src/tests/TestCase.php
@@ -3,8 +3,20 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Support\Facades\Artisan;
 
 abstract class TestCase extends BaseTestCase
 {
+    static $testInitialized = false;
+
     use CreatesApplication;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        if (!self::$testInitialized) {
+            Artisan::call('migrate');
+            self::$testInitialized = true;
+        }
+    }
 }
