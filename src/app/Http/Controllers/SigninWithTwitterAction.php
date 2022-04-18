7d6119b2ad7c1635c8ec38d920c97a69894fa9f5
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-04-18 23:03:34 +0900

    added signin with twitter action.

---
+++ src/app/Http/Controllers/SigninWithTwitterAction.php
@@ -0,0 +1,42 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Exceptions\ExceptionBaseClass;
+use App\Interfaces\Usecases\Signin\SigninAuthUserInterface;
+use App\ValueObjects\User\OauthProviderName;
+use Illuminate\Http\JsonResponse;
+use Illuminate\Support\Facades\Response;
+use Symfony\Component\ErrorHandler\Error\FatalError;
+use Throwable;
+
+class SigninWithTwitterAction extends Controller
+{
+    /**
+     * @var SigninAuthUserInterface
+     */
+    private SigninAuthUserInterface $usecase;
+
+    /**
+     * @param SigninAuthUserInterface $usecase
+     */
+    public function __construct(SigninAuthUserInterface $usecase)
+    {
+        $this->usecase = $usecase;
+    }
+
+    /**
+     * @return JsonResponse
+     */
+    public function __invoke(): JsonResponse
+    {
+        try {
+            $user = $this->usecase->process(OauthProviderName::TWITTER);
+        } catch (Throwable $e) {
+            throw new FatalError('Failed to signin.', ExceptionBaseClass::STATUS_CODE_INTERNAL_SERVER_ERROR, []);
+        }
+
+        return Response::json($user->toArray());
+    }
+}
--- src/routes/api.php
+++ src/routes/api.php
@@ -1,6 +1,6 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\SigninWithTwitterAction;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,6 +14,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::prefix('signin')->group(function() {
+    Route::prefix('twitter')->group(function() {
+        Route::get('/callback', SigninWithTwitterAction::class);
+    });
 });
