777086cc19d3bbe188d727939bb9a2461d3d8f37
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-04-22 22:17:39 +0900

    fixed signin test.

---
+++ src/database/seeders/UserSeeder.php
@@ -0,0 +1,27 @@
+<?php
+declare(strict_types=1);
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            'account_id' => 1,
+            'display_name' => 'test_display_name',
+            'avatar' => 'https://example.com/test_image.png',
+            'access_token' => 'test_access_token',
+            'refresh_token' => 'test_refresh_token',
+            'provider' => 'twitter',
+        ]);
+    }
+}
