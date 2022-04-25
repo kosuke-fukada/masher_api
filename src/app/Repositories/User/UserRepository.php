c74d93f7e7988b9744ddeffac91ead4f250c29e9
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-04-25 23:25:31 +0900

    added user repository.

---
+++ src/app/Repositories/User/UserRepository.php
@@ -0,0 +1,56 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Repositories\User;
+
+use App\Entities\User\UserInfo;
+use App\Interfaces\Repositories\User\UserRepositoryInterface;
+use App\ValueObjects\User\AccountId;
+use App\ValueObjects\User\OauthProviderName;
+use App\Models\User;
+
+class UserRepository implements UserRepositoryInterface
+{
+    /**
+     * @var User
+     */
+    private User $user;
+
+    /**
+     * @param User $user
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * @param AccountId $accountId
+     * @param OauthProviderName $provider
+     * @return User|null
+     */
+    public function findByAccountIdAndProvider(AccountId $accountId, OauthProviderName $provider): ?User
+    {
+        return $this->user->newQuery()
+            ->where('account_id', '=', (string)$accountId)
+            ->where('provider', '=', $provider->value)
+            ->first();
+    }
+
+    /**
+     * @param UserInfo $userInfo
+     * @return void
+     */
+    public function updateUser(UserInfo $userInfo): void
+    {
+        $targetUser = $this->user->newQuery()->find($userInfo->userId()->toInt());
+        $targetUser->fill([
+            'user_name' => (string)$userInfo->userName(),
+            'display_name' => (string)$userInfo->displayName(),
+            'avatar' => (string)$userInfo->avatar(),
+            'access_token' => (string)$userInfo->accessToken(),
+            'refresh_token' => (string)$userInfo->refreshToken(),
+        ]);
+        $targetUser->save();
+    }
+}
