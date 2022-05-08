b0e1a64237da15fe29ea4a935cb7a20238a5bbd7
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-05-08 21:20:21 +0900

    added tweet entity.

--- src/config/client.php
+++ src/config/client.php
@@ -5,6 +5,12 @@ return [
     'api' => [
         'twitter' => [
             'base_url' => env('TWITTER_API_BASE_URL', '')
+        ],
+        'twitter_oembed' => [
+            'base_url' => env('TWITTER_OEMBED_API_BASE_URL', '')
         ]
+    ],
+    'twitter' => [
+        'base_url' => env('TWITTER_BASE_URL', '')
     ]
 ];
