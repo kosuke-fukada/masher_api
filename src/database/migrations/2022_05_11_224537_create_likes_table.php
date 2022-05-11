31d49cc24b3848b83cc140568add22a1395e62d3
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-05-11 23:24:37 +0900

    added like model and table.

---
+++ src/database/migrations/2022_05_11_224537_create_likes_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('likes', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedInteger('user_id')->nullable(false);
+            $table->string('tweet_id')->nullable(false);
+            $table->string('author_id')->nullable(false);
+            $table->unsignedInteger('like_count')->nullable(false);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('likes');
+    }
+};
