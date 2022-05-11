a719d7c1ee007585e7029bedb579b3bb38de5281
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-05-11 23:25:32 +0900

    added like repository.

--- src/app/Providers/RepositoryServiceProvider.php
+++ src/app/Providers/RepositoryServiceProvider.php
@@ -3,7 +3,9 @@ declare(strict_types=1);
 
 namespace App\Providers;
 
+use App\Interfaces\Repositories\Like\LikeRepositoryInterface;
 use App\Interfaces\Repositories\User\UserRepositoryInterface;
+use App\Repositories\Like\LikeRepository;
 use App\Repositories\User\UserRepository;
 use Illuminate\Support\ServiceProvider;
 
@@ -15,5 +17,6 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register()
     {
         $this->app->singleton(UserRepositoryInterface::class, UserRepository::class);
+        $this->app->singleton(LikeRepositoryInterface::class, LikeRepository::class);
     }
 }
---
+++ src/tests/Repositories/Like/LikeRepositoryTest.php
@@ -0,0 +1,51 @@
+<?php
+declare(strict_types=1);
+
+namespace Tests\Repositories\Like;
+
+use Tests\TestCase;
+use App\Models\Like;
+use App\ValueObjects\User\UserId;
+use App\ValueObjects\Tweet\TweetId;
+use App\ValueObjects\Like\LikeCount;
+use App\ValueObjects\Shared\AccountId;
+use App\Repositories\Like\LikeRepository;
+use App\Interfaces\Repositories\Like\LikeRepositoryInterface;
+
+class LikeRepositoryTest extends TestCase
+{
+    /**
+     * @return LikeRepositoryInterface
+     */
+    public function test__construct(): LikeRepositoryInterface
+    {
+        $likeRepository = $this->app->make(LikeRepositoryInterface::class);
+        $this->assertInstanceOf(LikeRepository::class, $likeRepository);
+        return $likeRepository;
+    }
+
+    /**
+     * @depends test__construct
+     * @param LikeRepositoryInterface $likeRepository
+     * @return void
+     */
+    public function testCreateLike(LikeRepositoryInterface $likeRepository): void
+    {
+        $userId = 1;
+        $tweetId = '1';
+        $accountId = '1';
+        $likeCount = 1;
+        $like = new \App\Entities\Like\Like(
+            new UserId($userId),
+            new TweetId($tweetId),
+            new AccountId($accountId),
+            new LikeCount($likeCount)
+        );
+        $created = $likeRepository->createLike($like);
+        $this->assertInstanceOf(Like::class, $created);
+        $this->assertSame($userId, (int)$created->getAttribute('user_id'));
+        $this->assertSame($tweetId, (string)$created->getAttribute('tweet_id'));
+        $this->assertSame($accountId, (string)$created->getAttribute('author_id'));
+        $this->assertSame($likeCount, (int)$created->getAttribute('like_count'));
+    }
+}
