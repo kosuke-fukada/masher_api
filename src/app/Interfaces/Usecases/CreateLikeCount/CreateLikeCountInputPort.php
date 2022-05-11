ed41646497db4cd56525ff90e12d9da28675b05a
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-05-11 23:26:13 +0900

    added create like count input.

---
+++ src/app/Interfaces/Usecases/CreateLikeCount/CreateLikeCountInputPort.php
@@ -0,0 +1,38 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Interfaces\Usecases\CreateLikeCount;
+
+use App\ValueObjects\Like\LikeCount;
+use App\ValueObjects\Like\LikeIdentifier;
+use App\ValueObjects\Shared\AccountId;
+use App\ValueObjects\Tweet\TweetId;
+use App\ValueObjects\User\UserId;
+
+interface CreateLikeCountInputPort
+{
+    /**
+     * @return LikeIdentifier|null
+     */
+    public function likeIdentifier(): ?LikeIdentifier;
+
+    /**
+     * @return UserId
+     */
+    public function userId(): UserId;
+
+    /**
+     * @return TweetId
+     */
+    public function tweetId(): TweetId;
+
+    /**
+     * @return AccountId
+     */
+    public function accountId(): AccountId;
+
+    /**
+     * @return LikeCount
+     */
+    public function likeCount(): LikeCount;
+}
