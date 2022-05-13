63b00588b2c3349b7c459c99eaa0e58d8fda5a26
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-05-14 00:48:21 +0900

    added get like count input.

---
+++ src/app/Interfaces/Usecases/Like/GetLikeCount/GetLikeCountInputPort.php
@@ -0,0 +1,26 @@
+<?php
+declare(strict_types=1);
+
+namespace App\Interfaces\Usecases\Like\GetLikeCount;
+
+use App\ValueObjects\Shared\AccountId;
+use App\ValueObjects\Tweet\TweetId;
+use App\ValueObjects\User\UserId;
+
+interface GetLikeCountInputPort
+{
+    /**
+     * @return UserId
+     */
+    public function userId(): UserId;
+
+    /**
+     * @return TweetId
+     */
+    public function tweetId(): TweetId;
+
+    /**
+     * @return AccountId
+     */
+    public function accountId(): AccountId;
+}
