d80346a88810ae4e395731451e1c9d8c43a179cc
kosuke-fukada <j.lee.style.boogie.piano@gmail.com>
2022-05-23 00:21:37 +0900

    added author id value object.

---
+++ src/app/ValueObjects/Tweet/AuthorId.php
@@ -0,0 +1,33 @@
+<?php
+declare(strict_types=1);
+
+namespace App\ValueObjects\Tweet;
+
+use InvalidArgumentException;
+use App\ValueObjects\Foundation\StringValueObject;
+
+class AuthorId extends StringValueObject
+{
+    /**
+     * @param string $value
+     */
+    public function __construct(string $value)
+    {
+        $this->validate($value);
+        $this->value = $value;
+    }
+
+    /**
+     * @param string $value
+     * @return void
+     */
+    protected function validate(string $value): void
+    {
+        if (mb_strlen($value) === 0) {
+            return;
+        }
+        if (!preg_match('/^[[:graph:]]+$/', $value)) {
+            throw new InvalidArgumentException('Included invalid characters.');
+        }
+    }
+}
